f431253dc0cdd4d5aeff0d7d87cb8460506b464e
SvetlanaRomaniuk111 <126294774+SvetlanaRomaniuk111@users.noreply.github.com>
2023-09-30 20:07:00 +0300

    submit hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,77 +1,60 @@
 import { fetchImages } from 'api/api';
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import Button from './Button';
 import ImageGallery from './ImageGallery';
 import { Loader } from './Loader';
 import SearchBar from './Searchbar';
 
 export const App = () => {
-  const [state, setState] = useState({
-    gallery: null,
-    filtedGalery: null,
-    query: '',
-    page: 1,
-    err: '',
-    isLoading: true,
-  });
-
-  useEffect(() => {
-    setState({ isLoading: true });
-    fetchImages(state.page, state.query).then(data => {
-      setTimeout(() => {
-        setState(prev => ({
-          gallery: [...prev.gallery, ...data.hits],
-          isLoading: false,
-        }));
-      }, 500);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [state.page]);
+  const [gallery, setGallery] = useState([]);
+  const [query, setQuery] = useState('');
+  const [totalHits, setTotalHits] = useState(null);
+  const [page, setPage] = useState(1);
+  const [err, setErr] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleLoadMore = () => {
-    setState(prev => ({ page: prev.page + 1 }));
+    setPage(page + 1);
+    setIsLoading(true);
   };
 
   const submit = value => {
-    fetchImages(state.page, value)
-      .then(data => {
-        setState({ gallery: data.hits, query: value });
-      })
-      .catch(err => {
-        setState({ err, isLoading: false });
-      });
+    setPage(1);
+    setIsLoading(true);
+    setQuery(value);
+    setGallery([]);
+    setTotalHits(null);
   };
 
   useEffect(() => {
-    if (!Boolean(state.query)) {
-      setState({ gallery: [], isLoading: false });
-    }
-    fetchImages(state.page)
-      .then(({ hits }) => {
+    fetchImages(page, query)
+      .then(data => {
         setTimeout(() => {
-          state.query
-            ? setState({ gallery: hits, isLoading: false })
-            : setState({ gallery: [], isLoading: false });
+          setGallery(prev => (query ? [...prev, ...data.hits] : []));
+          setTotalHits(data.totalHits);
+          setIsLoading(false);
         }, 500);
       })
       .catch(err => {
-        setState({ err, isLoading: false });
+        setErr(err);
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [page, query]);
 
   return (
     <div className="App">
       <SearchBar submit={submit} />
-      {state.isLoading ? (
+      {isLoading ? (
         <Loader />
+      ) : err ? (
+        <h1>{err}</h1>
       ) : (
         <>
-          <ImageGallery gallery={state.gallery} />
+          <ImageGallery gallery={gallery} />
           <Button
             loadMore={handleLoadMore}
-            isRender={state.gallery.length}
-            page={state.page}
+            isRender={gallery.length && totalHits > gallery.length}
+            page={page}
           />
         </>
       )}
